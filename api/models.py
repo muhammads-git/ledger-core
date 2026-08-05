dee1d71a9675ad3a2b58c59636236f358d6d8f45
muhammads-git <roadprogrammar@gmail.com>
2026-08-05 13:06:09 +0500

    schema design 2

--- api/models.py
+++ api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String,Column,BigInteger,Integer,DateTime,ForeignKey
+from sqlalchemy import String,Column,BigInteger,Integer,DateTime,ForeignKey,Enum
 from sqlalchemy.orm import sessionmaker,mapped_column,Mapped
 from api.database import BASE
 import uuid
@@ -8,7 +8,8 @@ class User(BASE):
    __tablename__ = 'users'
 
    id = Column(Integer,primary_key=True,autoincrement=True)
-   name = Column(String,index=True,nullable=False)
+   first_name = Column(String,index=True,nullable=False)
+   last_name = Column(String,)
    email = Column(String,index=True,nullable=False)
    password = Column(String,nullable=False)
    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
@@ -31,4 +32,21 @@ class Account(BASE):
     currency = Column(String(3), default='PKR', nullable=False) 
     
     # Timezone-aware creation time
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+
+class AccountTypeCode(Enum):
+    CHECKINGS = 'CHECKINGS'
+    SAVINGS = 'SAVINGS'       
+    SYSTEM_FEES = 'SYSTEM_FEES'
+
+# 2. Your AccountType Table
+class AccountType(BASE):
+    __tablename__ = 'account_types'
+    
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False) # e.g., "Standard Checking"
+    
+    # .value so SQLAlchemy stores the raw string ('CHECKINGS') in the database
+    code = Column(String(20), default=AccountTypeCode.CHECKINGS.value, nullable=False)
+    
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
