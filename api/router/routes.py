5886e63091a9ac30a89158531539a3b1463874d2
muhammads-git <roadprogrammar@gmail.com>
2026-08-11 12:46:28 +0500

    fix file name

--- api/router/routes.py
+++ api/router/routes.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,HTTPException,Depends,APIRouter
 from sqlalchemy.orm import Session
 from api.database import get_db
-from api.scehma.schema import TransferCredentials
+from api.schema.schema import TransferCredentials
 from api.models import Account,AccountType,Transaction,LedgerEntries
 
 
